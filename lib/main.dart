f34270e3d1b7ae125d0eca9f50388e562701af3b
Jahidul Islam <shukanpukuripec@gmail.com>
2025-02-05 23:07:37 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AddEmployeeScreen(),
+    );
+  }
+}
+
+class AddEmployeeScreen extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController salaryController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Employee'),
+      ),
+      body: SingleChildScrollView(  // Added this
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: ageController,
+                decoration: InputDecoration(labelText: 'Age'),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: salaryController,
+                decoration: InputDecoration(labelText: 'Salary'),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  // Add employee logic
+                },
+                child: Text('Add Employee'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
